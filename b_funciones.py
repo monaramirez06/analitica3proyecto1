252f345bdba4798485a57201a9d46689dc963a89
allisonpg12 <alpiedrahitaga@unal.edu.co>
2023-09-06 22:23:57 -0500

    Archivo de funciones

---
+++ b_funciones.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.model_selection import cross_validate
+from sklearn.feature_selection import SelectFromModel
+
+# Función para realizar la validación cruzada de los modelos
+def cross_validation(model, _X, _y, _cv=5):
+      _scoring = ['accuracy', 'precision', 'recall', 'f1']
+      results = cross_validate(estimator=model,
+                               X=_X,
+                               y=_y,
+                               cv=_cv,
+                               scoring=_scoring,
+                               return_train_score=True)
+
+      return {"Training Accuracy scores": results['train_accuracy'],
+              "Mean Training Accuracy": results['train_accuracy'].mean()*100,
+              "Training Precision scores": results['train_precision'],
+              "Mean Training Precision": results['train_precision'].mean(),
+              "Training Recall scores": results['train_recall'],
+              "Mean Training Recall": results['train_recall'].mean(),
+              "Training F1 scores": results['train_f1'],
+              "Mean Training F1 Score": results['train_f1'].mean(),
+              "Validation Accuracy scores": results['test_accuracy'],
+              "Mean Validation Accuracy": results['test_accuracy'].mean()*100,
+              "Validation Precision scores": results['test_precision'],
+              "Mean Validation Precision": results['test_precision'].mean(),
+              "Validation Recall scores": results['test_recall'],
+              "Mean Validation Recall": results['test_recall'].mean(),
+              "Validation F1 scores": results['test_f1'],
+              "Mean Validation F1 Score": results['test_f1'].mean()
+              }
+
+# Función para seleccionar las variables importantes
+def sel_variables(modelos,X,y,threshold):
+
+    var_names_ac=np.array([])
+    for modelo in modelos:
+        #modelo=modelos[i]
+        modelo.fit(X,y)
+        sel = SelectFromModel(modelo, prefit=True,threshold=threshold)
+        var_names= modelo.feature_names_in_[sel.get_support()]
+        var_names_ac=np.append(var_names_ac, var_names)
+        var_names_ac=np.unique(var_names_ac)
+
+    return var_names_ac
